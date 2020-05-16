af006745eb0d3f933a728b0df676634cd645fbcc
Lucas Schürer <39245000+LucasSchurer@users.noreply.github.com>
2020-05-16 18:57:09 -0300

    Background adicionado

---
+++ JogoExemplo/Assets/Scripts/Background.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Transform posicaoJogador;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(posicaoJogador.position.x, transform.position.y);
+    }
+}
